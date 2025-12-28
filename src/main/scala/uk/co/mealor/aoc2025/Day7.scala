10d465dd184b9e4582868c281a7f542a6f028d42
Paul Mealor <paul.mealor@gmail.com>
2025-12-28 00:58:43 +0000

    Day 7 part 1

---
+++ src/main/scala/uk/co/mealor/aoc2025/Day7.scala
@@ -0,0 +1,174 @@
+package uk.co.mealor.aoc2025
+
+import org.scalatest.funsuite.AnyFunSuiteLike
+
+import scala.annotation.tailrec
+import scala.collection.immutable
+
+object Day7 {
+
+  @main
+  def runDay7() = {
+
+    println(part1(scala.io.Source.fromResource("day7-part1.txt").getLines()))
+    println(part2(scala.io.Source.fromResource("day7-part1.txt").getLines()))
+
+  }
+
+
+  def at[T](m: Int => Option[Int => Option[T]], x: Int, y: Int, d: T): T = {
+    m(y).flatMap(_(x)).getOrElse(d)
+  }
+
+  def findChars(line: String, toFind: Char): Set[Int] = {
+    Set.from(line.zipWithIndex.filter((c, i) => c == toFind).map((c, i) => i))
+  }
+
+  def initialBeam(line: String): Set[Int] = {
+    findChars(line, 'S')
+  }
+
+  def findSplitters(line: String): Set[Int] = {
+    findChars(line, '^')
+  }
+
+  @tailrec
+  def propagate(splits: Int, beams: Set[Int], lines: Iterator[String]): (Int, Set[Int]) = {
+    if lines.isEmpty then
+      (splits, beams)
+    else {
+
+      val splitters = findSplitters(lines.next())
+
+      val newSplits = splitters.count(beams.contains)
+      val newBeams = beams.flatMap(ib => if splitters.contains(ib) then Iterable(ib - 1, ib + 1) else Iterable(ib))
+
+      (0 until newBeams.max.max(splitters.maxOption.getOrElse(0)) + 1)
+        .foreach(i => print(if splitters.contains(i) && newBeams.contains(i) then {
+          "X"
+        } else if splitters.contains(i) then {
+          "^"
+        } else if newBeams.contains(i) then {
+          "|"
+        } else {
+          "."
+        }))
+      println(" " + newSplits)
+
+      propagate(splits + newSplits, newBeams, lines)
+    }
+  }
+
+  def part1(lines: Iterator[String]): Long = {
+
+    val beams = initialBeam(lines.next())
+
+    propagate(0, beams, lines)(0)
+  }
+
+  def part2(lineIter: Iterator[String]): Long = {
+    0
+  }
+
+  def peek[T](x: T): T = {
+    println(x);
+    x
+  }
+
+
+}
+
+class Day7Test extends AnyFunSuiteLike {
+
+  test("day 7 part 1") {
+    assert(Day7.part1(
+      """.......S.......
+        |...............
+        |.......^.......
+        |...............
+        |......^.^......
+        |...............
+        |.....^.^.^.....
+        |...............
+        |....^.^...^....
+        |...............
+        |...^.^...^.^...
+        |...............
+        |..^...^.....^..
+        |...............
+        |.^.^.^.^.^...^.
+        |...............""".stripMargin.linesIterator)
+      === 21)
+  }
+
+  test("day 7 part 1 a") {
+    assert(Day7.part1(
+      """.......S.......""".stripMargin.linesIterator)
+      === 0)
+  }
+
+  test("day 7 part 1 b") {
+    assert(Day7.part1(
+      """.......S.......
+        |...............""".stripMargin.linesIterator)
+      === 0)
+  }
+
+  test("day 7 part 1 c") {
+    assert(Day7.part1(
+      """.......S.......
+        |...............
+        |.......^.......""".stripMargin.linesIterator)
+      === 1)
+  }
+
+  test("day 7 part 1 d") {
+    assert(Day7.part1(
+      """.......S.......
+        |...............
+        |.......^.......
+        |...............""".stripMargin.linesIterator)
+      === 1)
+  }
+
+  test("day 7 part 1 e") {
+    assert(Day7.part1(
+      """.......S.......
+        |...............
+        |.......^.......
+        |...............""".stripMargin.linesIterator)
+      === 1)
+  }
+
+  test("day 7 part 1 f") {
+    assert(Day7.part1(
+      """.......S.......
+        |...............
+        |.......^.......
+        |...............
+        |......^.^......""".stripMargin.linesIterator)
+      === 3)
+  }
+
+  test("day 7 part 2") {
+    assert(Day7.part2(
+      """.......S.......
+        |...............
+        |.......^.......
+        |...............
+        |......^.^......
+        |...............
+        |.....^.^.^.....
+        |...............
+        |....^.^...^....
+        |...............
+        |...^.^...^.^...
+        |...............
+        |..^...^.....^..
+        |...............
+        |.^.^.^.^.^...^.
+        |...............""".stripMargin.linesIterator)
+      === 3263827)
+  }
+
+}
